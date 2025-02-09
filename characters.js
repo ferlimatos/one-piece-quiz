74de94ebcd35f06edaa48049c114fa80fa7251c5
ferlimatos <fernandamatos.design@gmail>
2025-02-09 20:18:31 -0300

    Iniciando o jogo de adivinhação de One Piece

---
+++ characters.js
@@ -0,0 +1,77 @@
+/*  
+   Lista de personagens do jogo de adivinhação
+   Cada personagem tem um nome e dicas associadas.
+*/
+
+/*  
+   Baseado em pesquisas de popularidade realizadas entre os fãs de "One Piece", aqui estão alguns dos personagens mais populares:
+
+Monkey D. Luffy
+Roronoa Zoro
+Nami
+Sanji
+Trafalgar D. Water Law
+Nico Robin
+Boa Hancock
+Carrot
+Portgas D. Ace
+Sabo
+Yamato
+Shanks
+Donquixote Rosinante (Corazón)
+Charlotte Katakuri
+Usopp
+Tony Tony Chopper
+Jinbe
+Marco
+Eustass Kid
+Vinsmoke Reiju
+Perona
+Brook
+Dracule Mihawk
+Franky
+Kozuki Oden
+Yamato
+Enel
+Crocodile
+Smoker
+Buggy
+
+Esses personagens foram destacados em diversas pesquisas de popularidade realizadas entre os fãs de "One Piece".
+*/
+
+const characters = [
+    {
+        name: "Luffy",
+        hints: [
+          "É um pirata",
+          "Possui uma Akuma no Mi",
+          "Seu sonho é encontrar o One Piece",
+          "Tem um chapéu de palha",
+          "Capitão dos Chapéus de Palha"
+        ]
+      },
+      {
+        name: "Zoro",
+        hints: [
+          "Usa três espadas",
+          "Quer se tornar o melhor espadachim do mundo",
+          "Se perde facilmente",
+          "Faz parte dos Chapéus de Palha",
+          "Treinou com Mihawk"
+        ]
+      },
+      {
+        name: "Sanji",
+        hints: [
+          "É um excelente cozinheiro",
+          "Nunca usa as mãos para lutar",
+          "Tem uma perna muito forte",
+          "Faz parte dos Chapéus de Palha",
+          "Foi criado em um restaurante flutuante"
+        ]
+      }
+      // Adicione mais personagens aqui
+    ];
+    
+    export default characters;
